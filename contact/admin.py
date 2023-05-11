3ca071ed5118940399e0608dc9fe0baae49d4577
Reis567 <matheusdosreis9@gmail.com>
2023-05-10 21:09:05 -0300

    admin page modifications

--- contact/admin.py
+++ contact/admin.py
@@ -5,4 +5,10 @@ from contact.models import Contact
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = 'id','first_name', 'last_name', 'phone', 'email',
+    ordering = '-id',
+    search_fields = 'id' , 'first_name' , 'last_name' ,
+    list_per_page = 10 
+    list_max_show_all = 1000 
+    list_editable = 'first_name', 'last_name',
+    list_display_links = 'id','phone', 
