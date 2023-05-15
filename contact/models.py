74d85d54025e86d0f37ff39043141c52d118cf9c
Reis567 <matheusdosreis9@gmail.com>
2023-05-14 21:47:17 -0300

    Categorys creation

--- contact/models.py
+++ contact/models.py
@@ -2,7 +2,10 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
-
+class Category(models.Model):
+    name = models.CharField(max_length=50)]
+    def __str__(self) -> str:
+        return f'{self.name}'
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
@@ -13,7 +16,14 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True ,upload_to='pictures/%Y/%m')
+    category = models.ForeignKey(
+        Category, 
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+        )
     
+
+
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
 
