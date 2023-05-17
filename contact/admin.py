ed8872b2c74e7f1e4b64f699ea526687dff39d01
Reis567 <matheusdosreis9@gmail.com>
2023-05-17 20:09:31 -0300

    single contact

--- contact/admin.py
+++ contact/admin.py
@@ -6,12 +6,12 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id','first_name', 'last_name', 'phone', 'email',
+    list_display = 'id','first_name', 'last_name', 'phone', 'email','show',
     ordering = '-id',
     search_fields = 'id' , 'first_name' , 'last_name' ,
     list_per_page = 10 
     list_max_show_all = 1000 
-    list_editable = 'first_name', 'last_name',
+    list_editable = 'first_name', 'last_name','show',
     list_display_links = 'id','phone', 
     
 @admin.register(models.Category)
