8e9eb0a931dce2c0d167e9a24418421379f9e885
Reis567 <matheusdosreis9@gmail.com>
2023-05-22 08:44:00 -0300

    delete's url template and view

--- contact/urls.py
+++ contact/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from contact import views
 
+from contact import views
 
 app_name = 'contact'
 
@@ -12,6 +12,6 @@ urlpatterns = [
     path('contact/<int:contact_id>/detail/',views.contact, name='contact'),
     path('contact/create/',views.create, name='create'),
     path('contact/<int:contact_id>/update/',views.update, name='update'),
-    path('contact/<int:contact_id>/delete/',views.contact, name='contact'),
+    path('contact/<int:contact_id>/delete/',views.delete, name='delete'),
     
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -72,3 +72,5 @@ def contact(request,contact_id):
         'contact/contact.html',
         context
     )
+
+
